81dd86780da1cc46e820732f3ab441703571b8bd
alicemkyn <alicemkoyun@gmail.com>
2024-01-23 18:32:56 +0300

    Happy 2024, It is snowing in terminal!

---
+++ snow.py
@@ -0,0 +1,46 @@
+import os
+import random
+import time
+
+SNOW_DENSITY = 10
+DELAY = .1
+
+snowflakes = ['❆', '❃', '•', '·', '*']
+
+term = os.get_terminal_size()
+
+w = term.columns
+h = term.lines
+
+grid = []
+
+for _ in range(h):
+    grid.append([' '] * w)
+
+def draw_grid():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('\033[?25l') # Remove the cursor from terminal
+    
+    output = ''
+
+    for row in grid:
+        output += ''.join(row) + '\n'
+    
+    output = output.strip('\n')
+    
+    print(output, end='')
+
+while True:
+    row = []
+    
+    for _ in range(w):
+        if random.random() < SNOW_DENSITY / 100:
+            row.append(random.choice(snowflakes))
+        else:
+            row.append(' ')
+            
+    grid.insert(0, row)
+    grid.pop()
+    draw_grid()
+    
+    time.sleep(DELAY)
